1fb8bd9e2838a2830d9daefd7cffe78674e66c37
navesnicolle <navesnicolle@gmail.com>
2023-03-18 14:13:40 -0300

    projeto final

--- js/calculadora.js
+++ js/calculadora.js
@@ -6,12 +6,12 @@ function somar() {
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
   
+const soma = numero1 + numero2
   
   
   
   
-  
-  document.getElementById("resultadoSoma").innerHTML = 10;
+  document.getElementById("resultadoSoma").innerHTML = soma;
 }
 
 function subtrair() {
@@ -22,12 +22,12 @@ function subtrair() {
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
   
+  const subtrair = numero1 - numero2
   
   
   
   
-  
-  document.getElementById("resultadoSub").innerHTML = 9;
+  document.getElementById("resultadoSub").innerHTML = subtrair;
 }
 
 function multiplicar() {
@@ -38,10 +38,10 @@ function multiplicar() {
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
   
+  const multiplicar = numero1 * numero2
   
   
-  
-  document.getElementById("resultadoMult").innerHTML = 5;
+  document.getElementById("resultadoMult").innerHTML = multiplicar;
 }
 
 function dividir() {
@@ -52,9 +52,9 @@ function dividir() {
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
   
+  const dividir = numero1 / numero2
   
   
   
-  
-  document.getElementById("resultadoDiv").innerHTML = -89;
+  document.getElementById("resultadoDiv").innerHTML = dividir;
 }
